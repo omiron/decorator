1530fd41c377ebb68ff684defdc6d980261111b8
Ovidiu Miron <ovidiu@madrivo.com>
2013-06-27 19:44:17 +0300

    added member function

--- sketch.cpp
+++ sketch.cpp
@@ -2,20 +2,32 @@
 class DataMember
 {
     public:
-        void *data;
+        void *data;//data member
         std::string data_type;//keeps the type of data(like "int" or "MyClass")
 };
+
+class FunctionMember
+{
+    public:
+        void * (* member)(void *); //function member
+}
 
 /*the dynamic object. */
 class DynObject
 {
     //map<name of the member, DataMember
     std::map<std::string, DataMember> members
+    std::map<std::string, FunctionMember> functions; 
     public:
         //register a new data member to DynObject
         bool add(void* _data, char* _data_type, char *member_name);
         //return the DataMember corespunding to member_name
         DataMember* call(char *member_name);
+        
+        bool add_function(void * (* _function)(void *), char *function_name);
+        FunctionMember* call_function(char *member_name);
+
+
 };
 
 bool DynObject::add(void* _data, char* _data_type, char *member_name)
@@ -28,6 +40,8 @@ bool DynObject::add(void* _data, char* _data_type, char *member_name)
     return members.insert(std::pair<std::string, DataMember>(member_name, new_member));
 }
 
+// bool DynObject::add_function(void * (* _function)(void *), char *function_name);
+// similar with add
 
 DataMember* DynObject::call(char *_name)
 {
@@ -35,6 +49,9 @@ DataMember* DynObject::call(char *_name)
 }
 
 
+//FunctionMember* call_function(char *member_name);
+//similar with call
+//
 int main()
 {
     DynObject dyObj;
@@ -51,13 +68,22 @@ int main()
         DataMember dm = dynObj.call(dynamic_choose);
         switch(dynamic_choose):
         {
-            case "int": &(static_cast<*int>)(dm->data) = 7;
-            case "MyClass":&(dynamic_cast<*MyClass>)(dm->data) = MyClass(32, "age"); 
+            case "int": *(static_cast<*int>)(dm.data) = 7;
+            case "MyClass":*(dynamic_cast<*MyClass>)(dm.data) = MyClass(32, "age"); 
         }
         
     }
     catch(exception &e)
     {
         ......
+    }
+
+    //#include my_function.h
+    //my_function(int, int)
+    dynObj.add_function(&my_function, "my_function");
+    try
+    {
+        FunctionMember fm = dynObj.call_function("my_function");
+        (*(my_function *)fm)(23, 23);
     }
 }
